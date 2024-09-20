f41ea599cd0c9923739e99dbbe88f294f937f025
Guilhermedos <159535759+alvarodograu@users.noreply.github.com>
2024-09-20 08:38:44 -0300

    attalvaro

--- nomedoseuapp/App.tsx
+++ nomedoseuapp/App.tsx
@@ -5,11 +5,6 @@ import {View,Text,StyleSheet} from 'react-native';
 export default function app (){
   return(
     <View>
-      <View>
-     
-      </View>
-      <View>
-
-      </View>
+      <Text></Text>
     </View>
 )}
